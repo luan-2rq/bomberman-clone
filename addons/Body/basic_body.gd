f18f0dac1bd55806d710c8f72bf15c9ae237f7d2
Eduardo Brancher Urenha <edu.urenha93@gmail.com>
2020-04-06 21:43:04 -0300

    Adicionado basic_body
    A ideia é que basic_body seja um novo tipo de nodo que inclua KinematicBody2D e as hitboxes e colisões e sirva desse modo para implementar todas as funçoes que envolvem mover algum objeto.
    Idealmente tanto os players quanto a bomba herdariam os metodos de basic_body.

---
+++ addons/Body/basic_body.gd
@@ -0,0 +1,18 @@
+tool
+extends KinematicBody2D
+
+
+var collides = 0 #determina se o objeto colide com outros ou não.
+var weight = 0 #poder necessario para levantar e jogar o objeto.
+
+func _enter_tree():
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
